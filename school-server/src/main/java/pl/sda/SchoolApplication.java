009a4e7fca10904114e9fc7c6fe12575da2c1ad7
Konrad Bartczak <konradbartczak@onet.pl>
2018-06-24 12:12:28 +0200

    To check.

--- school-server/src/main/java/pl/sda/SchoolApplication.java
+++ school-server/src/main/java/pl/sda/SchoolApplication.java
@@ -2,8 +2,10 @@ package pl.sda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "pl.sda.*")
+@EntityScan("pl.sda.*")
 public class SchoolApplication {
 
 	public static void main(String[] args) {
