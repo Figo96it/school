96bdf35780a1a30731e9c26ce0e16caca40f00ab
mborkowski13 <m.borkowski13@gmail.com>
2018-06-24 14:04:55 +0200

    Added class_id to student

--- school-server/src/main/resources/sql/student.sql
+++ school-server/src/main/resources/sql/student.sql
@@ -1,5 +1,6 @@
 create table student (
   STUDENT_ID int auto_increment not null,
+  class_id int not null,
   FIRST_NAME varchar(20) not null,
   LAST_NAME varchar(20) not null,
   primary key (STUDENT_ID)
