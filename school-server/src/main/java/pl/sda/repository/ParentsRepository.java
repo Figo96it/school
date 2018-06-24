9a2b26f3551bff6bd7c1ba7ff4fa9a242f79b531
Konrad Bartczak <konradbartczak@onet.pl>
2018-06-24 15:52:36 +0200

    Fix Parent Repository

--- school-server/src/main/java/pl/sda/repository/ParentsRepository.java
+++ school-server/src/main/java/pl/sda/repository/ParentsRepository.java
@@ -2,8 +2,8 @@ package pl.sda.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import pl.sda.model.Parents;
+import pl.sda.model.Parent;
 
 @Repository
-public interface ParentsRepository extends CrudRepository<Parents, Integer> {
+public interface ParentsRepository extends CrudRepository<Parent, Integer> {
 }
