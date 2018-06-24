c84e47b55a7ac88f1474fdae6e0adcd1104e5c17
lukasz komorowski <komorozo87@gmail.com>
2018-06-24 11:00:20 +0200

    create model repository
    (cherry picked from commit a56d461)

---
+++ school-server/src/main/java/pl/sda/repository/ParentsRepository.java
@@ -0,0 +1,9 @@
+package pl.sda.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.sda.model.Parents;
+
+@Repository
+public interface ParentsRepository extends CrudRepository<Parents, Integer> {
+}
---
+++ school-server/src/main/java/pl/sda/repository/SchoolRepository.java
@@ -0,0 +1,10 @@
+package pl.sda.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.sda.model.School;
+
+
+@Repository
+public interface SchoolRepository extends CrudRepository<School,Integer> {
+}
