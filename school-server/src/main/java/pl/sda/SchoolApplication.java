a8d92d85c642547eb6f987fc345837d2c923afef
Damian <futymadamiano@gmail.com>
2018-06-30 15:00:55 +0200

    *Added data-h2 file with fake data to db *Minor changes in schema

--- school-server/src/main/java/pl/sda/SchoolApplication.java
+++ school-server/src/main/java/pl/sda/SchoolApplication.java
@@ -1,14 +1,29 @@
 package pl.sda;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-
-import static org.springframework.boot.SpringApplication.run;
+import pl.sda.repository.GradeRepository;
+import javax.sql.DataSource;
 
 @SpringBootApplication(scanBasePackages = "pl.sda.*")
 @EntityScan("pl.sda.*")
-public class SchoolApplication {
+public class SchoolApplication implements CommandLineRunner {
+
+    @Autowired
+    GradeRepository gradeRepository;
+
+    @Autowired
+    DataSource dataSource;
+
     public static void main(String[] args) {
-        run(SchoolApplication.class, args);
+        SpringApplication.run(SchoolApplication.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        System.out.println(gradeRepository.count());
     }
 }
--- school-server/src/main/java/pl/sda/tool/DataInitTool.java
+++ school-server/src/main/java/pl/sda/tool/DataInitTool.java
@@ -1,18 +1,13 @@
 package pl.sda.tool;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import pl.sda.mocks.MockDataResolver;
-import pl.sda.model.Grade;
-import pl.sda.model.Parent;
-import pl.sda.model.Student;
 import pl.sda.repository.GradeRepository;
 import pl.sda.repository.ParentRepository;
 import pl.sda.repository.StudentRepository;
 
 import javax.annotation.PostConstruct;
-import java.util.List;
+import javax.sql.DataSource;
 
 
 @Component
@@ -21,26 +16,31 @@ public class DataInitTool {
     private GradeRepository gradeRepository;
     private ParentRepository parentRepository;
     private StudentRepository studentRepository;
+    private DataSource dataSource;
 
     @Autowired
-    public DataInitTool(GradeRepository gradeRepository, ParentRepository parentRepository, StudentRepository studentRepository) {
+    public DataInitTool(GradeRepository gradeRepository, ParentRepository parentRepository, StudentRepository studentRepository, DataSource dataSource) {
         this.gradeRepository = gradeRepository;
         this.parentRepository = parentRepository;
         this.studentRepository = studentRepository;
+        this.dataSource = dataSource;
     }
 
     @PostConstruct
     public void initData() {
-        List<Student> students = MockDataResolver.findAllStudents();
-        List<Parent> parents = MockDataResolver.findAllParents();
-        List<Grade> grades = MockDataResolver.findAllGrades();
-
-        try {
-            studentRepository.save(students);
-            parentRepository.save(parents);
-            gradeRepository.save(grades);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+//        List<Student> students = MockDataResolver.findAllStudents();
+//        List<Parent> parents = MockDataResolver.findAllParents();
+//        List<Grade> grades = MockDataResolver.findAllGrades();
+//
+//        try {
+//            studentRepository.save(students);
+//            parentRepository.save(parents);
+//            gradeRepository.save(grades);
+//        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//        }
+        System.out.println(studentRepository.count());
+        System.out.println(parentRepository.count());
+        System.out.println(gradeRepository.count());
     }
 }
