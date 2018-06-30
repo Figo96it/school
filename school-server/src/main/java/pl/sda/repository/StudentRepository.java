343f3e418a06e84a067d8c392d4d0aa71d7b19bf
sdaldz5_3 <sdaldz5_3@wp.pl>
2018-06-30 10:17:27 +0000

    StudentRepository.java edited online with Bitbucket

--- school-server/src/main/java/pl/sda/repository/StudentRepository.java
+++ school-server/src/main/java/pl/sda/repository/StudentRepository.java
@@ -2,10 +2,7 @@ package pl.sda.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import pl.sda.model.Parent;
 import pl.sda.model.Student;
-
-import java.util.List;
 
 @Repository
 public interface StudentRepository extends CrudRepository<Student, Integer> {
